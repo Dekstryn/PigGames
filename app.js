e09fe7d17e283ed537dc957209f2f2d897ef165f
KrzysztofNyrek <nitrobenzen@gmail.com>
2019-11-25 22:03:59 +0100

    Set css style when player win

--- app.js
+++ app.js
@@ -52,8 +52,11 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
   //Update the UI
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
   //Check if palyer win the game
-  if (scores[activePlayer] >= 100){
-    document.querySelector('#name-1' + activePlayer).textContent = 'Winner';
+  if (scores[activePlayer] >= 10){
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
   }
   else{
     nextplayer();
