a7949fe47e28e09b29080ad0c480f5e17e3bbab3
KrzysztofNyrek <nitrobenzen@gmail.com>
2019-11-25 21:45:55 +0100

    Add switching current player after result storage

--- app.js
+++ app.js
@@ -60,5 +60,14 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
   scores[activePlayer] += roundScore;
   //Update the UI
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
   //Check if palyer win the game
 });
